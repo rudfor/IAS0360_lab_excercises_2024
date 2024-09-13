f201295ffcc52b84036d22fecbd69cff7222e7b2
ishikawatatsuki <t.iokinawa.531@gmail.com>
2024-09-13 16:07:09 +0300

    Adding cyw43_arch for pico W

--- lab_0/mcu/main.cpp
+++ lab_0/mcu/main.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <vector>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 #include "includes/AnyLib.h"
-
-const uint LED_PIN = 25;
 
 void task1() 
 {
@@ -17,12 +16,15 @@ void task1()
 int main()
 {
     stdio_init_all();
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN,GPIO_OUT);
+    // Initialize the system and Wi-Fi chip (which includes the onboard LED control)
+    if (cyw43_arch_init()) {
+        // Failed to initialize
+        return -1;  
+    }
     bool value =0;
     while (true)
     {
-        gpio_put(LED_PIN,value);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, value);
         sleep_ms(500);
         value =!value;
         printf("LED toggle\n");
